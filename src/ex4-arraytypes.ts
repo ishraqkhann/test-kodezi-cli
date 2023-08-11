2f05d72bd6481e2389bd32405190778e9c7be4c8
Kodezi <info@kodezi.com>
2023-08-11 03:48:05 +0000

    commit my files of path src/ex4-arraytypes.ts

--- src/ex4-arraytypes.ts
+++ src/ex4-arraytypes.ts
@@ -1,11 +1,9 @@
 interface MyMap {
 	[index: string]: string;
 }
-
 let translations: MyMap = {
 	ok: 'Alright'
 };
-
 // XXX This does not work because all pairs must match
 let anotherMap: MyMap = {
 	'0': 'first'
