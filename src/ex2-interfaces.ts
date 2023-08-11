292e511a387649a2902afdf9d554708638e105c2
Kodezi <info@kodezi.com>
2023-08-11 03:48:15 +0000

    commit my files of path src/ex2-interfaces.ts

--- src/ex2-interfaces.ts
+++ src/ex2-interfaces.ts
@@ -3,12 +3,19 @@ export interface Drawable {
 	name?: string;
 	draw: (target: Node) => void;
 }
-
+/**
+ * Represents a rectangle that can be drawn on a target node.
+ */
 class Rect implements Drawable {
 	id: number;
+
+	/**
+	 * Draws the rectangle on a target node.
+	 *
+	 * @param target - The node where the rectangle will be drawn.
+	 */
 	draw(target: Node) {}
 }
-
 const thing: Drawable = {
 	id: 0,
 	draw: function (target: Node) { }
