5a365b7a8143d8a176cfe46a3d8a9e2d6352952b
Kodezi <info@kodezi.com>
2023-08-11 03:48:21 +0000

    commit my files of path tests/support/util.ts

--- tests/support/util.ts
+++ tests/support/util.ts
@@ -4,7 +4,14 @@
 export interface Thenable<T> {
 	then<U>(onFulfilled?: (value?: T) => U | Thenable<U>, onRejected?: (error?: any) => U | Thenable<U>): Thenable<U>;
 }
-
+/**
+ * Checks if a promise is eventually rejected.
+ *
+ * @export
+ * @template T - The type of the promise.
+ * @param {Thenable<T>} promise - The promise to check.
+ * @returns {Thenable<boolean>} - A promise that resolves to true if the input promise is rejected, and throws an error otherwise.
+ */
 export function isEventuallyRejected<T>(promise: Thenable<T>): Thenable<boolean> {
 	return promise.then<any>(function () {
 		throw new Error('unexpected code path');
@@ -12,8 +19,11 @@ export function isEventuallyRejected<T>(promise: Thenable<T>): Thenable<boolean>
 		return true; // expect rejection
 	});
 }
-
+/**
+ * Immediately throws an error with a specific message.
+ *
+ * @throws {Error} When this function is called.
+ */
 export function throwImmediatly() {
 	throw new Error('unexpected code path');
 }
-
