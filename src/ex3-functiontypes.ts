f573394f6935d2ab56ff7d9efc9a27e65db571de
Kodezi <info@kodezi.com>
2023-10-26 11:40:43 +0000

    commit my files of path src/ex3-functiontypes.ts

--- src/ex3-functiontypes.ts
+++ src/ex3-functiontypes.ts
@@ -7,11 +7,24 @@ interface DrawMethod {
  * 
  * @param {any} target - The target to draw on.
  */
-let method: DrawMethod = function(target: any) { };
+let method: DrawMethod = function(target: any) {
+    /**
+     * Represents a drawing method.
+     *
+     * @param {any} target - The target to draw on.
+     */
+};
 /**
  * Represents a method for drawing.
  * 
  * @param target The target to draw on.
  * @param options Optional parameters for the drawing method.
  */
-let anotherMethod: DrawMethod = function(target: any, options?: Object) { };
+let anotherMethod: DrawMethod = function(target: any, options?: Object) {
+    /**
+     * This is a method for drawing on a target with optional parameters.
+     * 
+     * @param {any} target - The target to draw on.
+     * @param {Object} [options] - Optional parameters for the drawing method.
+     */
+};
