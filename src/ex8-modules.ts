546135291d9757f22c4f9509ef8134ab6a2dc270
Kodezi <info@kodezi.com>
2023-11-20 09:14:20 +0000

    commit my files of path src/ex8-modules.ts

--- src/ex8-modules.ts
+++ src/ex8-modules.ts
@@ -3,6 +3,11 @@ import Animal, { Phylum, Dog } from './ex7-classes';
  * Represents a Cat, which is a specific type of Animal.
  */
 class Cat extends Animal {
-
+    /**
+     * Creates a new instance of Cat.
+     */
+    constructor() {
+        super();
+    }
 }
 const myPet = new Dog('Barky');
