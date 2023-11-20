94d8d5bc7765124a3d5ccaf6ef700f844c31722b
Kodezi <info@kodezi.com>
2023-11-20 09:14:25 +0000

    commit my files of path tests/intern-local.ts

--- tests/intern-local.ts
+++ tests/intern-local.ts
@@ -1,9 +1,25 @@
 export * from './intern';
+/**
+ * Exported variable representing the tunnel type.
+ * 
+ * @type {string}
+ */
 export var tunnel = 'NullTunnel';
+/**
+ * Options for a tunnel connection.
+ * 
+ * @property {string} hostname - The hostname of the tunnel server.
+ * @property {string} port - The port of the tunnel server.
+ */
 export var tunnelOptions = {
 	hostname: 'localhost',
 	port: '4444'
 };
+/**
+ * An array of environments for testing. Each environment is an object with a `browserName` property.
+ * 
+ * @type {Array}
+ */
 export var environments = [
 	{ browserName: 'chrome' }
 ];
